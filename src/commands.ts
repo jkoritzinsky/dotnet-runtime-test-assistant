fde4ea96e9e834dc121e1c17cb0c69610f01bcfe
Jeremy Koritzinsky <jekoritz@microsoft.com>
2022-08-03 13:27:27 -0700

    Add command to import settings from the devcontainer config file. These settings are settings the dotnet/runtime infra team already feels are good defaults, so provide an easy way to import them into a local repo's settings.

--- src/commands.ts
+++ src/commands.ts
@@ -1,6 +1,29 @@
+import path = require('path');
 import * as vscode from 'vscode';
+import { parse } from 'jsonc-parser';
+import { getRuntimeWorkspaceFolder } from './userPrompts';
+import { readFile } from 'fs/promises';
 
-// Import settings from the devcontainer configuration file into the local .vscode/settings.json file.
+// Import settings from the devcontainer configuration file into the local .vscode/settings.json file
+// if the setting is not already specified.
 export async function importSettingsFromDevContainer() {
-
+    const workspace = await getRuntimeWorkspaceFolder();
+    if (!workspace) {
+        return;
+    }
+    const devContainerConfig = parse(await readFile(path.join(workspace.fsPath, '.devcontainer', 'devcontainer.json'), 'utf-8'));
+    const devContainerSettings = devContainerConfig.settings;
+    const configuration = vscode.workspace.getConfiguration();
+    for (const setting in devContainerSettings) {
+        if (Object.prototype.hasOwnProperty.call(devContainerSettings, setting)) {
+            let value = devContainerSettings[setting];
+            if (typeof value === 'string') {
+                value = value.replace('${containerWorkspaceFolder}', '${workspaceFolder}');
+            }
+            let configurationResult = configuration.inspect<any>(setting);
+            if (!configurationResult || (!configurationResult.workspaceValue && !configurationResult.workspaceFolderValue)) {
+                await configuration.update(setting, value);
+            }
+        }
+    }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -4,11 +4,13 @@ import * as crossgen2CoreLibTestProvider from './providers/crossgen2-corelib';
 import * as crossgen2TestProvider from './providers/crossgen2';
 import * as libsNativeTestProvider from './providers/libs-native';
 import { setServerPathFromExtensionContext, getOrStartServerConnection } from './server';
+import { importSettingsFromDevContainer } from './commands';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('dotnet-runtime-test-assistant.startServerIfNotStarted', getOrStartServerConnection));
+	context.subscriptions.push(vscode.commands.registerCommand('dotnet-runtime-test-assistant.importSettingsFromDevContainer', importSettingsFromDevContainer));
 
 	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider(runtimeTestProvider.DEBUG_CONFIGURATION_TYPE, runtimeTestProvider, vscode.DebugConfigurationProviderTriggerKind.Dynamic));
 	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider(runtimeTestProvider.DEBUG_CONFIGURATION_TYPE, runtimeTestProvider));
